fb05832e45ec387108695a5756e0af2d862c9f80
DanniB2023 <daniellenichole820@yahoo.com>
2021-10-24 19:34:07 -0400

    KDL

--- html/main.js
+++ html/main.js
@@ -33,3 +33,86 @@ window.onload = () =>{
         document.querySelector('.header .header-2').classList.remove('active'); 
     }
 }
+
+
+
+
+
+$(document).ready(function () {
+    TweenLite.from('.firstCol', 2.5, {
+        ease: Power2.easeOut,
+        x: 400
+    });
+    TweenLite.to('.firstCol', 0, {
+        ease: Power2.easeOut,
+        x: 0
+    });
+    TweenLite.from('.fifthCol', 2.5, {
+        ease: Power3.easeOut,
+        x: -400
+    });
+    TweenLite.to('.fifthCol', 0, {
+        ease: Power3.easeOut,
+        x: 100
+    });
+    TweenLite.from('.secondColOne', 2.5, {
+        ease: Power3.easeOut,
+        x: 300,
+        y: 100
+    });
+    TweenLite.to('.secondColOne', 0, {
+        ease: Power3.easeOut,
+        x: 100,
+        y: 100
+    });
+    TweenLite.from('.secondColTwo', 2.5, {
+        ease: Power3.easeOut,
+        x: 300,
+        y: 0
+    });
+    TweenLite.to('.secondColTwo', 0, {
+        ease: Power3.easeOut,
+        x: 100,
+        y: 100
+    });
+    TweenLite.from('.thirdColOne', 2.5, {
+        ease: Power3.easeOut,
+        x: 0,
+        y: 300
+    });
+    TweenLite.to('.thirdColOne', 0, {
+        ease: Power3.easeOut,
+        x: 100,
+        y: 100
+    });
+    TweenLite.from('.thirdColThree', 2.5, {
+        ease: Power3.easeOut,
+        x: 0,
+        y: -300
+    });
+    TweenLite.to('.thirdColThree', 0, {
+        ease: Power3.easeOut,
+        x: 100,
+        y: 100
+    });
+    TweenLite.from('.fourthColOne', 2.5, {
+        ease: Power3.easeOut,
+        x: -300,
+        y: 300
+    });
+    TweenLite.to('.fourthColOne', 0, {
+        ease: Power3.easeOut,
+        x: 100,
+        y: 100
+    });
+    TweenLite.from('.fourthColTwo', 2.5, {
+        ease: Power3.easeOut,
+        x: -300,
+        y: 0
+    });
+    TweenLite.to('.fourthColTwo', 0, {
+        ease: Power3.easeOut,
+        x: 100,
+        y: 100
+    });
+});
